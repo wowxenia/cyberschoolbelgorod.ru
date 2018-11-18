dff9683a5ea4d3bbbe30e73e25eb8f00033b5b37
wowxenia <40713947+wowxenia@users.noreply.github.com>
2018-11-18 21:51:11 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@ var pos = 0;
 var transform = Modernizr.prefixed('transform');
 
 function setTransform() {
- items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)';
+	items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)';
 }
 
 function prev() {
- pos = Math.max(pos - 1, 0);
- setTransform();
+	pos = Math.max(pos - 1, 0);
+	setTransform();
 }
 function next() {
- pos = Math.min(pos + 1 , itemCount - 1);
- setTransform();
+	pos = Math.min(pos + 1, itemCount - 1);
+	setTransform();
 }
 window.addEventListener('resize', setTransform);﻿
