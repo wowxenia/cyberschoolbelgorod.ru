7bfe55f8c20e58ee671769ccad18028043ee51ba
wowxenia <40713947+wowxenia@users.noreply.github.com>
2018-11-26 04:27:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+
+
+		$(function(){
+			$('a[href^="#"]').click(function(){
+				var target = $(this).attr('href');
+				$('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+				return false; 
+			}); 
+		});
+
